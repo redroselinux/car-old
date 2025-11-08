098d3b4827e2ae998b09ef16105aa27ac3fc907f
Juraj Kollár <mostypc7@gmail.com>
2025-11-08 21:13:49 +0100

    Update get_package_list.py

--- src/get_package_list.py
+++ src/get_package_list.py
@@ -2,17 +2,11 @@ from status import status
 import os
 
 def main(package):
-    status("Looking for packages...")
-    
-    print("(1/3) Reading packagelist")
     with open("/home/"+os.getlogin()+"/.config/car/packagelist", "r") as f:
         packages = f.read()
 
-    print("(2/3) Loading packagelist...")
     packagelist = packages.splitlines()
-    print(packagelist)
 
-    print("(3/3) Checking for package...")
     if package in packagelist:
         return True
     else:
