48e2bb79641f29a5bccf59c3afaafef52d7ab988
Juraj Kollár <mostypc7@gmail.com>
2025-12-07 11:09:13 +0100

    Update lspkgs.py

--- src/lspkgs.py
+++ src/lspkgs.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    with open(f"/home/{os.getlogin()}/.config/repro.car", "r") as f:
+    with open("/etc/repro.car", "r") as f:
         repro = f.read()
 
     alr_used = []
