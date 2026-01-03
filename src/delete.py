166ff6e2b0ad322888e7179ab869ca72061b35b3
Juraj Kollár <mostypc7@gmail.com>
2026-01-03 20:56:40 +0100

    Update delete.py

--- src/delete.py
+++ src/delete.py
@@ -1,7 +1,7 @@
 import os
-import sys
 
 from rich.console import Console
+from rich.progress import BarColumn, Progress, TextColumn
 
 import umbrella.autocorrect_package as Autocorrect
 from status import status
@@ -10,9 +10,6 @@ console = Console()
 
 
 def main(package):
-    print(
-        "note that this function is not finished, and it does not delete all package files."
-    )
     autocorrected = Autocorrect.main(package)
     if autocorrected != package:
         package = autocorrected
@@ -38,14 +35,27 @@ def main(package):
         f.write(repro)
 
     status(f"Uninstalling {package}...", "info")
-    try:
-        if os.system(f"sudo rm -f /usr/bin/{package}") != 0:
-            status(f"Failed to uninstall {package}", "error")
-            return False
-        status(f"Successfully uninstalled {package}", "ok")
-        return True
-    except Exception:
-        console.print("::", style="red", end=" ")
-        console.print("Unhandled exception")
-        console.print_exception()
-        return False
+
+    delete_files = []
+
+    if os.path.isfile(f"/etc/car/saves/{package}"):
+        with open(f"/etc/car/saves/{package}", "r") as f:
+            delete_files = f.read().splitlines()
+
+    # +1 to account for /usr/bin/{package}
+    total = len(delete_files) + 1
+
+    status("Progress:")
+
+    with Progress(
+        TextColumn("{task.completed}/{task.total}"),
+        BarColumn(),
+    ) as progress:
+        task = progress.add_task("Deleting", total=total)
+
+        for i in delete_files:
+            os.system(f"rm -rf {i}")
+            progress.advance(task)
+
+        os.system(f"rm -f /usr/bin/{package}")
+        progress.advance(task)
