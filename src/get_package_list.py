f0f2bc3f4607b15bf66326898235df05c26c20be
Juraj Kollár <mostypc7@gmail.com>
2026-02-08 12:55:36 +0100

    h

--- src/get_package_list.py
+++ src/get_package_list.py
@@ -6,9 +6,11 @@ def main(package):
 
     packagelist = packages.splitlines()
 
+    # packagelist has "repo/pkg" format; package is always short name
     if package in packagelist:
         return True
-    else:
-        status("Package not found.", "error")
-        print("Maybe you forgot to update your packagelist?")
-        print("     car updatelist")
+    if any(pkg.split("/", 1)[-1] == package for pkg in packagelist if pkg.strip()):
+        return True
+    status("Package not found.", "error")
+    print("Maybe you forgot to update your packagelist?")
+    print("     car updatelist")
